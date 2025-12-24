261e368885af502e3951c6f515f33a35d2b443f8
Cristian Custodio <cristian@endtimeinvestments.com>
2025-12-24 08:20:50 -0600

    Set up Husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,23 +1,23 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import { Button } from "@/components/ui/button";
+import { useState, useEffect } from 'react';
+import './App.css';
+import { Button } from '@/components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState<string>("");
+   const [message, setMessage] = useState<string>('');
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error("Error:", error));
-  }, []);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((response: Response) => response.json())
+         .then((data: { message: string }) => setMessage(data.message))
+         .catch((error) => console.error('Error:', error));
+   }, []);
 
-  return (
-    <div className="p-4">
-      <p className="p-4 font-bold text-3xl">{message}</p>
-      <Button>Click me</Button>
-    </div>
-  );
+   return (
+      <div className="p-4">
+         <p className="p-4 font-bold text-3xl">{message}</p>
+         <Button>Click me!</Button>
+      </div>
+   );
 }
 
 export default App;
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,21 +1,21 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig, type PluginOption } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, type PluginOption } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()] as PluginOption[],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+   plugins: [react(), tailwindcss()] as PluginOption[],
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
       },
-    },
-  },
+   },
+   server: {
+      proxy: {
+         '/api': {
+            target: 'http://localhost:3000',
+         },
+      },
+   },
 });
