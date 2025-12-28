51dd0c3d913194178990240a52c132b6fd9c487a
Cristian Custodio <cristian@endtimeinvestments.com>
2025-12-28 12:15:33 -0600

    Finished chatbot with error handling

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,8 @@
-import { useState, useEffect } from 'react';
-import './App.css';
 import ChatBot from './components/ChatBot';
 
 function App() {
-   const [message, setMessage] = useState<string>('');
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((response: Response) => response.json())
-         .then((data: { message: string }) => setMessage(data.message))
-         .catch((error) => console.error('Error:', error));
-   }, []);
-
    return (
-      <div className="p-4">
+      <div className="p-4 h-screen w-full">
          <ChatBot />
       </div>
    );
