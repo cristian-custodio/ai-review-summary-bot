6d0d9025110b7f6eee0f7e8c1159bcbb9d128843
Cristian Custodio <cristian@endtimeinvestments.com>
2025-12-29 15:04:47 -0600

    Added initial endpoint for retrieving user reviews

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -5,6 +5,9 @@ import router from './routes';
 
 dotenv.config();
 
+// Log DATABASE_URL for verification (password will be visible)
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
 const app = express();
 app.use(express.json());
 app.use(router);
